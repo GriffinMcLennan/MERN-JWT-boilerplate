a89ef8ca7c09f98104e54318a54f7ce4684602d9
Griffin McLennan <mclennangriffin@gmail.com>
2020-10-08 23:59:08 -0500

    Added jsonwebtoken added cookieParser middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cookieParser);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
